e598655c15029b2f3abb634ac6176ec480cff80f
asad <asadtahir9999@gmail.com>
2022-08-07 12:36:52 -0500

    Added gym asset interface and abstract.

---
+++ Asset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop
+{
+    public interface Asset
+    {
+        public string Name { get; set; }
+        public Decimal Value { get; set; }
+    }
+}
---
+++ GymAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop
+{
+    public abstract class GymAsset
+
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public GymAsset(string nameOfAsset, decimal valueOfAsset)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = nameOfAsset;
+            Value = valueOfAsset;
+        }
+    }
+}
